a8c4e422357d9ab4d5292223c795c90179da1173
sample <youngjae7981@gmail.com>
2023-02-16 11:28:08 +0900

    sda

---
+++ script.js
@@ -0,0 +1,24 @@
+// const colorsArray = [[["red", "blue", "pink"]]];
+// // const materialsArray = ["plastic", "paper"];
+// // 라인마다 콘솔로그 찍어보기!
+// // ↑ 배열
+// // 이중 Array
+// console.log(colorsArray[0][0][0]);
+
+// 최대한 가독성이 좋게 만들어야함
+// 3중첩 x = 가독성 떨어짐
+//
+let a = [1, 2, 3];
+let b = a;
+b[0] = 5;
+
+// let value1 = 100;
+// value2 = value1;
+
+// 값 복사
+
+console.log(a);
+console.log(b);
+
+// value,reperence
+// Reference(참조):값이 아님 값을 알려주는게 아니고 공동체
