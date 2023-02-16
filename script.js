0a8c58cf8a3c0b4b16bcd2a3464bc112d67c447f
sample <youngjae7981@gmail.com>
2023-02-16 13:38:44 +0900

    dasdsa

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
-// const colorsArray = [[["red", "blue", "pink"]]];
-// // const materialsArray = ["plastic", "paper"];
-// // 라인마다 콘솔로그 찍어보기!
-// // ↑ 배열
-// // 이중 Array
-// console.log(colorsArray[0][0][0]);
+// 객체(object)
+// 자바스크립트 웬만해선 객체
 
-// 최대한 가독성이 좋게 만들어야함
-// 3중첩 x = 가독성 떨어짐
-//
-let a = [1, 2, 3];
-let b = a;
-b[0] = 5;
-
-// let value1 = 100;
-// value2 = value1;
-
-// 값 복사
-
-console.log(a);
-console.log(b);
-
-// value,reperence
-// Reference(참조):값이 아님 값을 알려주는게 아니고 공동체
+const user = {
+  name: "김영재",
+  age: 28,
+  like: ["축구", "게임", "야구"],
+  function() {
+    console.log(user.name, user.age, user.like);
+  },
+};
+user.function;
